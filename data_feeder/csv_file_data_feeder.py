549aa77440ca2414a8dbfdbee19f8f9763258029
Trevor Frey <trevor.frey@moat.com>
2018-08-22 13:42:29 -0400

    Setup info

--- data_feeder/csv_file_data_feeder.py
+++ data_feeder/csv_file_data_feeder.py
@@ -4,13 +4,15 @@ from constants.data_format import dict_to_data_point
 
 
 class CsvFileDataFeeder(DataFeeder):
-    """ A file-based implementation of DataFeeder"""
+    """ A file-based implementation of DataFeeder."""
 
     def __init__(self, filepath):
         self.filepath = filepath
 
     def get_data_stream(self):
-        """ Open the file that we are pointed to and yield the rows"""
+        """ Open the file that we are pointed to and yield the rows. This function is idempotent, although
+            there is no guarantee that other DataFeeders will be as well.
+        """
         with open(self.filepath) as f:
             reader = csv.DictReader(f)
 
