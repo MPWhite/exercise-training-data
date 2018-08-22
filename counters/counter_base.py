67cb23172b19050176df3ae9530b18bc1cf4d928
Trevor Frey <trevor.frey@moat.com>
2018-08-22 16:06:53 -0400

    Don't need data_feeder for counter, just a data_stream

--- counters/counter_base.py
+++ counters/counter_base.py
@@ -7,8 +7,6 @@ class Counter:
     """ An object that implements some version of counting for a given data stream produced by a data_feeder object.
         Various implementations of the counter can be built, but they should all have the `count_the_things` method
         that counts whatever features it's looking for out of the data_feeder's stream."""
-    # A counter has to have a data_feeder
-    data_feeder: DataFeeder
 
     def __init__(self, data_stream: Iterator[DataPoint]):
         self.data_stream = data_stream
